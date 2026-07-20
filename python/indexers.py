824087a1506fad44fad4eb9fb3f9943ccfab7f60
dphillips95 <david.phillips@fmi.fi>
2026-07-20 17:22:21 +0300

    Add copyright info

--- python/indexers.py
+++ python/indexers.py
@@ -1,4 +1,26 @@
-# Module of general tools, e.g. array rolling/sliding
+"""
+Indexing tools plus assorted other tools for pPIC.
+
+Copyright 2026 Finnish Meteorological Institute.
+
+This program is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program. If not, see
+<https://www.gnu.org/licenses/>.
+
+
+Author(s): David Phillips
+"""
 
 import numpy as np
 from numba import njit,int64
